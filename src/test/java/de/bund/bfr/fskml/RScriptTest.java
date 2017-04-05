1054f25582513873a0122c6acdd3952cc96f2334
miguelalba <Miguel.de-Alba-Aparicio@bfr.bund.de>
2017-04-05 16:33:52 +0200

    refactor RScriptTest

--- src/test/java/de/bund/bfr/fskml/RScriptTest.java
+++ src/test/java/de/bund/bfr/fskml/RScriptTest.java
@@ -1,23 +1,16 @@
 package de.bund.bfr.fskml;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-
-import de.bund.bfr.fskml.RScript;
 import junit.framework.TestCase;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
 
 /**
  * @author Miguel Alba
  */
 public class RScriptTest extends TestCase {
-
-	private final String origScript = "# This is a comment line: It should not appear in the simplified version\n"
-			+ "library(triangle)\n" + "source(other.R)\n"
-			+ "hist(result, breaks=50, main=\"PREVALENCE OF PARENT FLOCKS\")\n";
-	private RScript rScript;
 
 	public void testScript() throws IOException {
 		
@@ -25,20 +18,14 @@ public class RScriptTest extends TestCase {
 		File f = File.createTempFile("temp", "");
 		f.deleteOnExit();
 
-		try (FileWriter fw = new FileWriter(f)) {
-			fw.write(origScript);
-			fw.close();
-			rScript = new RScript(f);
-		}
+		String origScript = "# This is a comment line: It should not appear in the simplified version\n"
+				+ "library(triangle)\n" + "source(other.R)\n"
+				+ "hist(result, breaks=50, main=\"PREVALENCE OF PARENT FLOCKS\")\n";
+		FileUtils.writeStringToFile(f, origScript, "UTF-8");
+		RScript rScript = new RScript(f);
 
 		assertEquals(origScript, rScript.getScript());
-
-		List<String> expectedLibraries = Arrays.asList("triangle");
-		List<String> obtainedLibraries = rScript.getLibraries();
-		assertEquals(expectedLibraries, obtainedLibraries);
-
-		List<String> expectedSources = Arrays.asList("other.R");
-		List<String> obtainedSources = rScript.getSources();
-		assertEquals(expectedSources, obtainedSources);
+		assertEquals(Collections.singletonList("triangle"), rScript.getLibraries());
+		assertEquals(Collections.singletonList("other.R"), rScript.getSources());
 	}
 }
